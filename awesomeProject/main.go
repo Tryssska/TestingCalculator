3263a35f18325981354650c2bee327e76f953fa5
Tryssska <147539329+Tryssska@users.noreply.github.com>
2024-08-05 18:58:13 +0400

    main.go

--- awesomeProject/main.go
+++ awesomeProject/main.go
@@ -9,25 +9,20 @@ import (
 )
 
 func main() {
-
+	var romansCheck bool
+	
 	reader := bufio.NewReader(os.Stdin)
+	
 	for {
-
-		var (
-			firstDigit, secondDigit, operator string
-			splitInput                        []string
-			romansCheck                       = false
-		)
-
 		fmt.Print("Введите значения: ")
 		userInput, err := reader.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
 
-		userInput = strings.Trim(userInput, "\n")
-		splitInput = strings.Split(userInput, " ")
-		if len(splitInput) < 3 {
+		userInput = strings.TrimSpace(userInput)
+		splitInput := strings.Split(userInput, " ")
+		if len(splitInput) != 3 {
 			panic("неверный ввод")
 		}
 
@@ -35,13 +30,12 @@ func main() {
 			romansCheck = true
 		}
 
-		firstDigit, operator, secondDigit = splitInput[0], splitInput[1], splitInput[2]
+		firstDigit, operator, secondDigit := splitInput[0], splitInput[1], splitInput[2]
 
 		Utils.DigitsCheck(firstDigit, secondDigit, romansCheck, Utils.Romans)
 
 		res := Utils.Result(operator, firstDigit, secondDigit, romansCheck, Utils.Romans)
 
 		fmt.Println(res)
-
 	}
 }
