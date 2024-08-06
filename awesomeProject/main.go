b5d609e160e73fdcf38485e02eacb755aaa9ed77
Tryssska <147539329+Tryssska@users.noreply.github.com>
2024-08-06 22:04:00 +0400

    main.go

--- awesomeProject/main.go
+++ awesomeProject/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"awesomeProject/Utils"
+	"awesomeProject/utils"
 	"bufio"
 	"fmt"
 	"os"
@@ -9,12 +9,10 @@ import (
 )
 
 func main() {
-		
+
 	reader := bufio.NewReader(os.Stdin)
-	
 	for {
-		romansCheck := false
-		
+		IsRomans := false
 		fmt.Print("Введите значения: ")
 		userInput, err := reader.ReadString('\n')
 		if err != nil {
@@ -23,19 +21,14 @@ func main() {
 
 		userInput = strings.TrimSpace(userInput)
 		splitInput := strings.Split(userInput, " ")
-		if len(splitInput) != 3 {
-			panic("неверный ввод")
+
+		if _, ok := utils.Romans[splitInput[0]]; ok {
+			IsRomans = true
 		}
 
-		if _, ok := Utils.Romans[splitInput[0]]; ok {
-			romansCheck = true
-		}
+		firstDigit, operator, secondDigit := utils.GetDigits(splitInput, IsRomans)
 
-		firstDigit, operator, secondDigit := splitInput[0], splitInput[1], splitInput[2]
-
-		Utils.DigitsCheck(firstDigit, secondDigit, romansCheck, Utils.Romans)
-
-		res := Utils.Result(operator, firstDigit, secondDigit, romansCheck, Utils.Romans)
+		res := utils.Result(operator, firstDigit, secondDigit, IsRomans)
 
 		fmt.Println(res)
 	}
